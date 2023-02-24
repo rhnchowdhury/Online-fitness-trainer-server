a26cf7e5147ca5b21b84d307d04bdf9c9ccca731
Raihan <rhnchdri@gmail.com>
2023-02-25 00:28:22 +0600

    env file setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// Middle wares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ajoxj5t.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('database connected');
+    client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send('fitness server running');
+});
+
+app.listen(port, () => {
+    console.log(`fitness server running on port ${port}`);
+});
